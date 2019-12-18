c558f43d19fcc328e08b633a1186e30fa4086c9e
myciek <maciejmycielski@gmail.com>
2019-12-18 14:33:11 +0100

    Web version project folders

---
+++ LottoNet/src/java/exceptions/WrongArgumentsException.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.exceptions;
+
+/**
+ *Custom exception to work with arguments
+ * @author Maciek
+ */
+
+public class WrongArgumentsException
+     extends Exception {
+
+    /**
+     * Throw when arguments are incorrect
+     * @param errorMessage - Message that describe error
+     */
+    public WrongArgumentsException(String errorMessage) {
+        super(errorMessage);
+    }
+    
+}
